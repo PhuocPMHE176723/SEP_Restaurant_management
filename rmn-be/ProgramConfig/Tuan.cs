2150af146272aeca0accda345c72db357ab19fac
VuHHHE20704 <huyvu20704@gmail.com>
2026-02-27 00:37:05 +0700

    .

--- rmn-be/ProgramConfig/Tuan.cs
+++ rmn-be/ProgramConfig/Tuan.cs
@@ -5,7 +5,7 @@ namespace SEP_Restaurant_management.ProgramConfig
 {
     public static class Tuan
     {
-        public static IServiceCollection AddMyServices3(this IServiceCollection services)
+        public static IServiceCollection AddMyServices1(this IServiceCollection services)
         {
 
             
--- rmn-be/ProgramConfig/Vu.cs
+++ rmn-be/ProgramConfig/Vu.cs
@@ -4,7 +4,7 @@ namespace SEP_Restaurant_management.ProgramConfig
 {
     public static class Vu
     {
-        public static IServiceCollection AddMyServices4(this IServiceCollection services)
+        public static IServiceCollection AddMyServices2(this IServiceCollection services)
         {
             //services.AddScoped<IOrderRepository, OrderRepository>();
             //services.AddScoped<IOrderService, OrderService>();
