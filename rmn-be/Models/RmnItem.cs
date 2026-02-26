d9f87781daf86cdd3fed211d6b57f5515112f579
VuHHHE180800 <huyvu20704@gmail.com>
2026-02-27 00:07:56 +0700

    add models

--- rmn-be/Models/RmnItem.cs
+++
@@ -1,3 +0,0 @@
-namespace rmn_be.Models;
-
-public record RmnItem(Guid Id, string Title, string Description);
---
+++ rmn-be/ProgramConfig/Tuan.cs
@@ -0,0 +1,16 @@
+
+
+
+namespace SEP_Restaurant_management.ProgramConfig
+{
+    public static class Tuan
+    {
+        public static IServiceCollection AddMyServices3(this IServiceCollection services)
+        {
+
+            
+
+            return services;
+        }
+    }
+}
